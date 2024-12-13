badb4aaddba384e3e89714649a37af7fcf4bdf36
Арсений Кулагин <z1rden@MacBook-Pro-Arsenij.local>
2024-12-13 17:12:14 +0300

    Созданы структура проекта, фабричная функция, отлов ошибок с выводом соответствующих страниц, макет таблиц в базе данных.

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    #flask.config.from_object загружает настройки из указанного класса в объекте конфигурации Flask-приложения.
+    config[config_name].init_app(app)
+    #осуществляется конфигурация приложения
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI')
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
