cdac47ee37613005614cbc22448ffbeca3c240a2
Арсений Кулагин <z1rden@MacBook-Pro-Arsenij.local>
2024-12-14 14:45:16 +0300

    base

--- params_os.py
+++ params_os.py
@@ -4,3 +4,7 @@ import os
 os.environ['FLASK_CONFIG'] = 'development'
 os.environ['SECRET_KEY'] = 'SUPERSECRETKEY'
 os.environ['DEV_DATABASE_URI'] = 'postgresql://postgres:rowyourboat1@localhost/postgres'
+
+#export FLASK_CONFIG='development'
+#export SECRET_KEY='SUPERSECRETKEY'
+#export DEV_DATABASE_URI='postgresql://postgres:rowyourboat1@localhost/postgres'
