a32c7b06a625c16d2d7cf75d0386588c04ba022c
Арсений Кулагин <z1rden@MacBook-Pro-Arsenij.local>
2024-12-14 16:03:30 +0300

    Добавлена аутентификация без фронта и самой проверки, база данных обновлена, хэширование пароля и сохранение в базе данных добавлено

--- config.py
+++ config.py
@@ -5,19 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
 
-    """В будущем для использования почты
-    
-    MAIL_SERVER = 'smtp.yandex.ru'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
-                   ['true', 'on', '1']
-    MAIL_USERNAME = 'arseniykulagin@yandex.ru'
-    MAIL_PASSWORD = 'rowyourboat1'
-
-    #FLASKY_MAIL_SUBJECT_PREFIX = '[Flask-app]'
-    #FLASKY_MAIL_SENDER = 'Flask-app Admin <flask-app@example.com>'
-    #FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')"""
-
     @staticmethod
     def init_app(app):
         pass
--- manage.py
+++ manage.py
@@ -6,8 +6,6 @@ from flask_migrate import Migrate
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
-print(os.environ.get('FLASK_CONFIG'), os.environ.get('SECRET_KEY'), os.environ.get('DEV_DATABASE_URI'))
-
 
 if __name__ == '__main__':
     app.run()
