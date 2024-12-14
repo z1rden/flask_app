92d23f4176ceaeb41749b0f5a32dd06bbf196597
Арсений Кулагин <z1rden@MacBook-Pro-Arsenij.local>
2024-12-14 14:45:16 +0300

    Соединение с базой данных.

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 import os
 from app import create_app, db
-from app.models import User, Role
 from flask_migrate import Migrate
+
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+print(os.environ.get('FLASK_CONFIG'), os.environ.get('SECRET_KEY'), os.environ.get('DEV_DATABASE_URI'))
 
 
 if __name__ == '__main__':
