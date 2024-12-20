2fb31e4164985d39e74bcf320eefda23e1959be1
Арсений Кулагин <z1rden@MacBook-Pro-Arsenij.local>
2024-12-20 17:22:41 +0300

    Добавлена регистрация нового пользователя

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -15,6 +19,7 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
 
 class Role(db.Model):
@@ -12,9 +14,10 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
@@ -28,13 +31,27 @@ class User(db.Model):
     #затем setter (у них обязательно должны быть одинаковые названия функций)
     @password.setter
     def password(self, password):
-        password_hash = generate_password_hash(password)
+        password_hash = generate_password_hash(password, method='pbkdf2')
+        self.password_hash = password_hash
+        #в python, который поставляется с macos, отсутствует scrypt-модуль, поэтому добавляется method
+        #https://github.com/miguelgrinberg/microblog/issues/404
 
     #теперь для ПОПЫТКИ получения значения используется <объект_класса>.password
     #для изменения значения <объект_класса>.password = jdfsajf
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    #Flask - Login отслеживает зарегистрированного пользователя, сохраняя его уникальный идентификатор
+    #в пользовательском сеансе Flask, назначенный каждому пользователю, который подключается к приложению.
+    #Каждый раз, когда вошедший в систему пользователь переходит на новую страницу, Flask - Login
+    #извлекает идентификатор пользователя из сеанса и затем загружает этого пользователя в память.
+    #Поскольку Flask - Login ничего не знает о базах данных, ему нужна помощь приложения при загрузке
+    #пользователя. По этой причине расширение ожидает, что приложение настроит функцию загрузчика пользователя, которую
+    #можно вызвать для загрузки пользователя с идентификатором.
 
     def __repr__(self):
         return '<User %r>' % self.username
